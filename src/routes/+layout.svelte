2cbe170f069cc6ef9f0b86057686d9c930c0ad5f
Erik Sebastián de Erice <erikote2004@icloud.com>
2023-12-23 21:05:29 +0100

    Styling

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,32 @@
+<div>
+    <slot />
+</div>
+
+<style>
+    :global(*, *::before, *::after) {
+        box-sizing: border-box;
+    }
+
+    :global(body) {
+        display: grid;
+        place-content: center;
+        min-height: 100vh;
+        margin: 0;
+        background: #fdfdfd;
+        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+
+    :global(h1, h2, h3, h4, h5) {
+        margin: 0;
+    }
+
+    div {
+        aspect-ratio: 9 / 16;
+        width: 400px;
+        max-width: 100%;
+        position: relative;
+        display: block;
+        border-radius: 8px;
+        box-shadow: 0 0 100px rgba(0, 0, 0, .1);
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,45 @@
 <script>
     import { getWeatherFrom } from '../services/weather';
+    import Footer from '../components/Footer.svelte';
 
     const weatherPromise = getWeatherFrom();
 </script>
 
 {#await weatherPromise then weather}
-	<h1>{weather.conditionText}</h1>
+    <section>
+        <h1>{weather.name}</h1>
+        <h2>{weather.temperature}º</h2>
+        <h3>{weather.conditionText}</h3>
+        <h4>{weather.windSpeed}km/h</h4>
+    </section>
+
+    <Footer />
 {/await}
 
 <style>
+    section {
+        padding: 16px;
+    }
     h1 {
-        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        font-weight: 300;
+        color: #333;
+        text-transform: uppercase;
+        padding: 16px 0 0 0;
+    }
+
+    h2 {
+        font-weight: 300;
+        font-size: 100px;
+        color: #333;
+        text-transform: uppercase;
+        padding: 0;
+    }
+
+    h3 {
+        font-weight: 700;
+        transform: rotate(-90deg);
+        position: absolute;
+        top: 56px;
+        right: 12px;
     }
 </style>
